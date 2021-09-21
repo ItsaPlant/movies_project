b875de76df410f284595f52c819c5d91c6db25d0
ItsaPlant <krzysiekmichalik6@gmail.com>
2021-09-21 21:47:06 +0200

    c3

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template
+import tmdb_client
 
 app = Flask(__name__)
 
 @app.route("/")
 def homepage():
-    movies = range(20)
+    movies = tmdb_client.get_popular_movies()['results'][:8]
     return render_template("homepage.html", movies=movies)
 
 if __name__ == '__main__':
---
+++ tmdb_client.py
@@ -0,0 +1,20 @@
+import requests
+
+def get_popular_movies():
+    endpoint = "https://api.themoviedb.org/3/movie/popular"
+    api_token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNjY2OThiM2U2MThhNmNiYWU4ZDViYWE4YzJlODBkMSIsInN1YiI6IjYxNDljZDM3ZDZjMzAwMDAyYTQ2MWE4YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.eOu24_4pF0SitW_gYiGnXuIw6sN6rVtwZP3IpKlgW80"
+    headers = {
+        "Authorization": f"Bearer {api_token}"
+    }
+    responce = requests.get(endpoint, headers=headers)
+    return responce.json()
+    
+def get_poster_url(poster_api_path, size="w342"):
+    base_url = "https://image.tmdb.org/t/p/"
+    return f"{base_url}{size}/{poster_api_path}"   
+
+@app.context_processor
+def utility_processor():
+    def tmdb_image_url(path, size):
+        return tmdb_client.get_poster_url(path, size)
+    return {"tmdb_image_url": tmdb_image_url}
